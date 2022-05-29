166c37dc55fa284cee380c4b36ed8ec174687c5b
Bekbolat Kuralbayev <bkb@live.ru>
2022-05-29 23:16:34 +0600

    satisfy Triangle struct to Shape interface

--- triangle.go
+++ triangle.go
@@ -1,6 +1,18 @@
 package golang_united_school_homework
 
-// Triangle must satisfy to Shape interface
+import (
+	"math"
+)
+
+// Triangle is a struct that contains side and has methods for calculating perimeter and area.
 type Triangle struct {
 	Side float64
+}
+
+func (t *Triangle) CalcPerimeter() float64 {
+	return 3 * t.Side
+}
+
+func (t *Triangle) CalcArea() float64 {
+	return math.Sqrt(3) * t.Side * t.Side / 4
 }
