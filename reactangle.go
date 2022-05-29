f6cf2290808b0fcf31571d71045fffc20e845d3e
Bekbolat Kuralbayev <bkb@live.ru>
2022-05-29 23:16:17 +0600

    satisfy Rectangle struct to Shape interface

--- reactangle.go
+++ reactangle.go
@@ -1,7 +1,14 @@
 package golang_united_school_homework
 
-// Rectangle must satisfy to Shape interface
+// Rectangle is a struct that contains height and weight and has methods for calculating perimeter and area.
 type Rectangle struct {
 	Height, Weight float64
 }
 
+func (r *Rectangle) CalcPerimeter() float64 {
+	return 2 * (r.Height + r.Weight)
+}
+
+func (r *Rectangle) CalcArea() float64 {
+	return r.Height * r.Weight
+}
