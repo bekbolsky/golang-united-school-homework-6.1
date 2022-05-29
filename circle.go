7db0fae4dd0b57fe2f1ef32ec46f3db622fccde5
Bekbolat Kuralbayev <bkb@live.ru>
2022-05-29 23:15:41 +0600

    satisfy Circle struct to Shape interface

--- circle.go
+++ circle.go
@@ -1,6 +1,18 @@
 package golang_united_school_homework
 
-// Circle must satisfy to Shape interface
+import (
+	"math"
+)
+
+// Circle is a struct that contains radius and has methods for calculating perimeter and area.
 type Circle struct {
 	Radius float64
+}
+
+func (c *Circle) CalcPerimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+func (c *Circle) CalcArea() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
